defac3ff4ac02619374fac5e8b636d584aa21f57
kimgeunbae <naa159@naver.com>
2024-11-11 00:13:14 +0900

    React.memo까지

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-
-import { useEffect, useState } from 'react';
-import './App.css';
-import A from './component/A';
-import B from './component/B';
+import { useEffect, useState } from "react";
+import "./App.css";
+import A from "./component/A";
+import B from "./component/B";
 
 function App() {
-
   const [value, setValue] = useState("");
-  const [posts,setPosts] = useState([])
-  useEffect(()=>{
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(response => response.json())
-    .then(posts => setPosts(posts))
-  },[])
+      .then((response) => response.json())
+      .then((posts) => setPosts(posts));
+  }, []);
   return (
-    <div style={{padding: '1rem'}}>
-      <input value={value} onChange={e=>setValue(e.target.value)}/>
-      <div style={{display: 'flex'}}>
-        <A posts={posts} message={value}/>
-        <B/>
+    <div style={{ padding: "1rem" }}>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <div style={{ display: "flex" }}>
+        <A posts={posts} message={value} />
+        <B posts={posts} message={value} />
       </div>
     </div>
   );
--- src/component/B.js
+++ src/component/B.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
-function B() {
+const Message = React.memo(({ message }) => {
+  return <p>{message}</p>;
+});
+const ListItem = React.memo(({ post }) => {
   return (
-    <div>B</div>
-  )
-}
+    <li key={post.id}>
+      <p>{post.title}</p>
+    </li>
+  );
+});
+const List = React.memo(({ posts }) => {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <ListItem key={post.id} post={post} />
+      ))}
+    </ul>
+  );
+});
+const B = ({ message, posts }) => {
+  return (
+    <div>
+      <h1>B Component</h1>
+      <Message message={message} />
+      <List posts={posts} />
+    </div>
+  );
+};
 
-export default B
+export default B;
